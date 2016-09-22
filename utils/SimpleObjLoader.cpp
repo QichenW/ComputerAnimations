ec6a2c783aa5d8052b56d24d9ccf6d74f51af592
qichen <vincent.q.wang@gmail.com>
2016-09-22 02:09:28 -0400

    second commit

--- utils/SimpleObjLoader.cpp
+++ utils/SimpleObjLoader.cpp
@@ -7,69 +7,62 @@
 
 using namespace std;
 
-void loadObj() {
-    void loadObj(char *fname) {
-        FILE *fp;
-        int read;
-        GLfloat x, y, z;
-        char dump1,dump2;
-        unsigned long vIndex1,vIndex2,vIndex3, vnIndex;
-        char lineHeader[128];
-        char ch;
-        object = glGenLists(1);
-        fp = fopen(fname, "r");
-        if (!fp) {
-            printf("can't open file %s\n", fname);
-            exit(1);
-        }
-
-        glNewList(object, GL_COMPILE);
-        {
-            glPushMatrix();
-            // glBegin(GL_POINTS);
-            while (1) {
-                // read the first word of the line, if the return value is EOF then break
-                // %s ignores /r /0 /n in between lines
-                if (fscanf(fp, "%s", lineHeader) == EOF) {
-                    break;
-                }
-                if ( strcmp( lineHeader, "v" ) == 0 ){
-                    fscanf(fp, "%f %f %f", &x, &y, &z);
-                    // glVertex3f(x, y, z);
-                    vx.push_back(x);
-                    vy.push_back(y);
-                    vz.push_back(z);
-                }
-            }
-            // glEnd();
-            fclose(fp);
-            fp = fopen(fname, "r");
-            glLineWidth(1.0);
-            glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-            glBegin(GL_TRIANGLES);
-            while (1) {
-                // read the first word of the line, if the return value is EOF then break
-                // %s ignores /r /0 /n in between lines
-                if (fscanf(fp, "%s", lineHeader) == EOF) {
-                    break;
-                }
-                if ( strcmp( lineHeader, "f" ) == 0 ){
-
-                    fscanf(fp, "%lu %c %c %lu", &vIndex1, &dump1, &dump2, &vnIndex);
-                    fscanf(fp, "%lu %c %c %lu", &vIndex2, &dump1, &dump2, &vnIndex);
-                    fscanf(fp, "%lu %c %c %lu", &vIndex3, &dump1, &dump2, &vnIndex);
-                    glVertex3f(vx.at(vIndex1 - 1),vy.at(vIndex1 - 1), vz.at(vIndex1 - 1));
-                    glVertex3f(vx.at(vIndex2 - 1),vy.at(vIndex2 - 1), vz.at(vIndex2 - 1));
-                    glVertex3f(vx.at(vIndex3 - 1),vy.at(vIndex3 - 1), vz.at(vIndex3 - 1));
-                }
-            }
-            glEnd();
-            fclose(fp);
-
-
-        }
-        glPopMatrix();
-        glEndList();
-        fclose(fp);
+GLuint SimpleObjLoader::loadObj(char *fileName) {
+    FILE *filePointer;
+    GLfloat x, y, z;
+    GLuint object;
+    char dump1, dump2;
+    unsigned long vIndex1, vIndex2, vIndex3, vnIndex;
+    vector<GLfloat> vx, vy,vz;
+    char lineHeader[128];
+    object = glGenLists(1);
+    filePointer = fopen(fileName, "r");
+    if (!filePointer) {
+        printf("can't open file %s\n", fileName);
+        exit(1);
     }
+
+    glNewList(object, GL_COMPILE);
+
+        // glBegin(GL_POINTS);
+        while (1) {
+            // read the first word of the line, if the return value is EOF then break
+            if (fscanf(filePointer, "%s", lineHeader) == EOF) {
+                break;
+            }
+            if (strcmp(lineHeader, "v") == 0) {
+                fscanf(filePointer, "%f %f %f", &x, &y, &z);
+                // glVertex3f(x, y, z);
+                vx.push_back(x);
+                vy.push_back(y);
+                vz.push_back(z);
+            }
+        }
+        // glEnd();
+        fclose(filePointer);
+        filePointer = fopen(fileName, "r");
+        glLineWidth(1.0);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glBegin(GL_TRIANGLES);
+        while (true) {
+            // %s ignores /r /0 /n in between lines
+            if (fscanf(filePointer, "%s", lineHeader) == EOF) {
+                break;
+            }
+            if (strcmp(lineHeader, "f") == 0) {
+
+                fscanf(filePointer, "%lu %c %c %lu", &vIndex1, &dump1, &dump2, &vnIndex);
+                fscanf(filePointer, "%lu %c %c %lu", &vIndex2, &dump1, &dump2, &vnIndex);
+                fscanf(filePointer, "%lu %c %c %lu", &vIndex3, &dump1, &dump2, &vnIndex);
+                glVertex3f(vx.at(vIndex1 - 1), vy.at(vIndex1 - 1), vz.at(vIndex1 - 1));
+                glVertex3f(vx.at(vIndex2 - 1), vy.at(vIndex2 - 1), vz.at(vIndex2 - 1));
+                glVertex3f(vx.at(vIndex3 - 1), vy.at(vIndex3 - 1), vz.at(vIndex3 - 1));
+            }
+        }
+        glEnd();
+        fclose(filePointer);
+
+    glEndList();
+
+    return object;
 }
--- utils/SimpleObjLoader.h
+++ utils/SimpleObjLoader.h
@@ -2,13 +2,24 @@
 // Created by Qichen on 9/21/16.
 //
 
-#ifndef GLUTPROJECT_SIMPLEOBJHEADER_H
-#define GLUTPROJECT_SIMPLEOBJHEADER_H
+#ifndef GLUTPROJECT_SIMPLEOBJLOADER_H
+#define GLUTPROJECT_SIMPLEOBJLOADER_H
+
+// glut
+#if defined(__APPLE__)
+
+#include <GLUT/glut.h>
+#include <string>
+#include <vector>
+
+#else
+#include <GL/glut.h>
+#endif
 
 class SimpleObjLoader
 {
 public:
-    void loadObj();
+    GLuint static loadObj(char *fileName);
 };
 
 #endif //GLUTPROJECT_SIMPLEOBJHEADER_H
